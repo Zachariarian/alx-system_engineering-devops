4b886224c5ade8138e9b7894b386b7697d6243e4
Zachariarian <zachariarian@gmail.com>
2022-05-03 13:08:39 -0700

    poiu

--- 0-whatsmyname.c
+++ 0-whatsmyname.c
@@ -1,17 +1,17 @@
-#include <stdlib.h>
-#include <stdio.h>
-/**
- * main- a program that prints its name
- *
- * @argc: holds the number of arguments passed
- * @argv: array pointer that holds the arguments passed
- *
- * Return: Always 0 (Success)
- */
-int main(int __attribute__((unused)) argc, char *argv[])
+#include "main.h"
 
+#include <stdio.h>
+
+
+
+/**
+ * main - Prints the name of the program
+ * @argc: Number of command line arguments
+ * @argv: Array name
+ * Return: 0
+ */
+int main(int argc, char *argv[])
 {
-int i = 0;
-printf("%s\n", argv[i]);
+printf("%s\n", argv[argc * 0]);
 return (0);
 }
