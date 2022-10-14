0fbb2a5127210854e7f208432e8567d179364f97
Zachariarian <zachariarian@gmail.com>
2022-10-14 13:20:52 +0300

    tasks

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
-# Install a package
-package { 'puppet-lint':
-  ensure   => '2.1.0',
-  provider => 'flask',
+# install puppet-lint -v 2.5.0
+
+exec { 'puppet-lint':
+  command => '/usr/bin/apt-get -y install puppet-lint -v 2.5.0',
 }
